6a9c711c0c037df3cb952fdddddcb07d890afdca
sh1Nome <shinodayo1024@gmail.com>
2024-07-26 08:49:05 +0900

    update Swagger設定

--- swagger/swagger-codegen.sh
+++ swagger/swagger-codegen.sh
@@ -2,4 +2,5 @@
 docker run --rm -v ./work:/local swaggerapi/swagger-codegen-cli-v3 generate \
     -i /local/swagger.yml \
     -l spring \
-    -o /local/out/spring
+    -o /local/out/spring \
+    -c /local/config.json
